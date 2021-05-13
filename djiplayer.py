0fbf2e17167925dc772515cc92067253a3b9a2ab
Howard Cardwell <howard.cardwell@stratus-tis.com>
2021-05-13 14:21:51 -0400

    Base local control wrapper

--- djiplayer.py
+++ djiplayer.py
@@ -5,42 +5,60 @@ import os
 import tkinter as tk
 
 StartPlayer = True
-
-output = FIFOWriter.FIFOWriter()
-output.Open()
+StatusInterval = 0.5
 
 headset = USBDev.USBDev()
 headset.StartDevCheckThread()
 
-print("Writing to FIFO at " + output.FIFO)
+output = FIFOWriter.FIFOWriter()
+output.Open()
+
+# print("Writing to FIFO at " + output.FIFO)
+# State globals:
 BytesWritten = 0
+LastStatus = 0
+StatusData = dict()
+StatusData['Msg'] = "Starting Up"
 
+###############################################################################
 
+def PrintStatus(update):
+    """Update the program status"""
+    global LastStatus
+    now = time.time()
+    if (now - LastStatus) < StatusInterval:
+        return
+    print(update['Msg'], end='\r')
+    LastStatus = now
 
-def PrintStatus(StatusData):
-    print("Foo")
+###############################################################################
 
 while True:
+    PrintStatus(StatusData)
     if headset.DevicePresent == False:
-        print("Waiting on headset...")
+        # print("Waiting on headset...")
+        StatusData['Msg'] = "Waiting on headset..."
         time.sleep(.5)
         continue
 
     packet = headset.RecvData()
     if packet == None:
-        print("RecvData() returned None: {}".format(headset.LastError.strerror))
+        # print("RecvData() returned None: {}".format(headset.LastError.strerror))
+        StatusData['Msg'] = "Device State: " + headset.DeviceStatus
         continue
-    # out = output.Write(bytearray.fromhex("deadbeef00"))
+
     out = output.Write(packet)
 
     if out:
         BytesWritten += out
+        StatusData['Total'] = BytesWritten
     else:
-        print("Pausing output, no readers...")
+        # print("Pausing output, no readers...")
+        StatusData['Msg'] = "Output buffer full, pausing output..."
         time.sleep(.5)
         continue
 
-
-    print("In write loop, error: " + str(output.LastError) + ", total bytes written: " + str(BytesWritten))
+    # print("In write loop, error: " + str(output.LastError) + ", total bytes written: " + str(BytesWritten))
+    StatusData['Msg'] = "Device State: " + headset.DeviceStatus
     # time.sleep(.5)
     # exit()
