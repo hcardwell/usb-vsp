3cc31aa54ef319ab15bb5089ac5ceb29ff35943c
Howard Cardwell <howard.cardwell@stratus-tis.com>
2021-05-13 14:44:19 -0400

    Added player control logic

--- djiplayer.py
+++ djiplayer.py
@@ -2,18 +2,25 @@ from lib import USBDev
 from lib import FIFOWriter
 import time
 import os
-import tkinter as tk
+# import tkinter as tk
+import subprocess
 
 StartPlayer = True
 StatusInterval = 0.5
+
 
 headset = USBDev.USBDev()
 headset.StartDevCheckThread()
 
 output = FIFOWriter.FIFOWriter()
 output.Open()
-
 # print("Writing to FIFO at " + output.FIFO)
+
+# A bit of cheese.  Raspberry Pi OS:
+PlayCmd = "/usr/bin/ffplay -i " + output.FIFO + "-analyzeduration 1 -probesize 32 -sync ext"
+WrapPlayCmd = ["/usr/bin/lxterminal", "-e", PlayCmd]
+PlayerProc = None
+
 # State globals:
 BytesWritten = 0
 LastStatus = 0
@@ -33,8 +40,21 @@ def PrintStatus(update):
 
 ###############################################################################
 
+def StartPlayer():
+    global PlayCmd, WrapPlayCmd, PlayerProc
+
+    if PlayerProc:
+        PlayerProc.terminate()
+
+    PlayerProc = subprocess.Popen(WrapPlayCmd)
+
+###############################################################################
+
 while True:
     PrintStatus(StatusData)
+    if not PlayerProc:
+        StartPlayer()
+    
     if headset.DevicePresent == False:
         # print("Waiting on headset...")
         StatusData['Msg'] = "Waiting on headset..."
@@ -42,6 +62,8 @@ while True:
         if BytesWritten:
             BytesWritten = 0
             output.Reset()
+            StartPlayer()    # Really a restart, since we're unlinking the FIFO
+
         # No sense being speedy on the loop when we are waiting on a human
         time.sleep(.5)
         continue
