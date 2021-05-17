f4500138b37eefabaab2a522d60f35dae3f4d524
Howard Cardwell <howard.cardwell@stratus-tis.com>
2021-05-17 19:33:45 -0400

    Basic Win32 pipe writer working

---
+++ lib/Win32PipeWriter.py
@@ -0,0 +1,39 @@
+# Class wrapper for the ffplay read on Windows
+# Windows-specific counterpart to the FIFOWriter, which was quite
+# non-portable
+
+import time
+import sys
+import win32pipe, win32file, pywintypes
+
+name = "vsp-fifo"
+
+class Win32PipeWriter:
+    """Class wrapper for writing the DJI stream to a Win32 Named Pipe"""
+    def __init__(self):
+        self.FIFO = None
+        self.LastError = None
+        self.Handle = None
+
+
+    def Open(self, HandleName = name):
+        """Create and open a named pipe """
+        self.Handle = win32pipe.CreateNamedPipe(
+            r'\\.\pipe\vsp-fifo',
+            win32pipe.PIPE_ACCESS_DUPLEX,
+            win32pipe.PIPE_NOWAIT,
+            1, 65536, 65536,
+            0,
+            None)
+
+    def Write(self, Data):
+        if self.Handle == None:
+            self.LastError = "Pipe not open"
+            return None
+        rv = win32file.WriteFile(self.Handle, Data)
+        print("WriteFile returned " + str(rv))
+        return
+
+    def Reset(self):
+        return
+
